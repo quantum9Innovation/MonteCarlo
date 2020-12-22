68a8edb15786326ef6a915e5699e0a5ae73fbc70
quantum9innovation <46249765+quantum9Innovation@users.noreply.github.com>
2020-12-21 17:33:37 -0800

    Data has arrived

--- index.py
+++ index.py
@@ -8,6 +8,21 @@ client.remove_command('help')
 game = None
 
 
+class Monopoly(object):
+
+    def __init__(self, n_players):
+
+        self.player_cash = [1500] * n_players
+        self.player_locations = [0] * n_players  # 0 --> 40
+        self.properties_owned = [[]] * n_players
+        """
+        represented by tuple with: 
+         - strings (e.g `lb1` for Light Blue 1)
+         - houses purchased (hotel = 5 houses)
+        """
+        self.in_jail = [False] * n_players
+
+
 @client.command(pass_context=True)
 async def help(ctx):
 
